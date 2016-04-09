33edbb034f9fd2204eea7ee000df4337815ab3bc
SDK <s.danyal.k@gmail.com>
2016-04-08 23:51:00 -0500

    Added express server code

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var app = express();
+var port = 3000;
+
+/********************Routing********************/
+app.get("/", function (req, res) {
+    res.redirect('/public_html/index.html');
+})
+
+/********************Init*********************/
+app.listen(port, function () {
+    console.log('ABCarWebsite server listening on port 3000!');
+});
