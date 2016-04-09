b4aeb04d6a87a000b1e90d62c3678ab3925e7814
SDK <s.danyal.k@gmail.com>
2016-04-09 00:01:47 -0500

    updating

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 3000;
 
 /********************Routing********************/
 app.get("/", function (req, res) {
-    res.redirect('/public_html/index.html');
+    res.redirect('/index.html');
 })
 
 /********************Init*********************/
