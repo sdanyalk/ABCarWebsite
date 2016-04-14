1700391340daf882a9860f1b774c8032b7ec9a3d
SDK <s.danyal.k@gmail.com>
2016-04-13 20:56:09 -0500

    Added Express server code

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get("/", function (req, res) {
+    res.redirect('/public_html/index.html');
+});
+
+app.listen(port);
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
