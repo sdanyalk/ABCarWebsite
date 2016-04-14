2249796381851f6ef489d648d6ccfbfa9faf3aa9
SDK <s.danyal.k@gmail.com>
2016-04-13 22:52:12 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 3000;
 
 app.get("/", function (req, res) {
-    res.redirect('/ABCarWebsite/public_html/index.html');
+    res.redirect('/root/ABCarWebsite/public_html/index.html');
 });
 
 app.listen(port, function () {
