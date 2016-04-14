f84df391574ea2b79eebe90426539b20d0e89064
SDK <s.danyal.k@gmail.com>
2016-04-13 22:41:39 -0500

    updated port

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 ﻿var express = require('express');
 var app = express();
-var port = 4000;
+var port = 3000;
 
 app.get("/", function (req, res) {
     //res.redirect('/public_html/index.html');
     res.redirect('www.google.com');
 });
 
-app.listen(port);
-
 app.listen(port, function () {
-    console.log('Example app listening on port ' + port);
+    console.log('AIWebsite app listening on port ' + port);
 });
