1dd3bcb5b6312e1a890ceb6e42c4e257491e1215
SDK <s.danyal.k@gmail.com>
2016-04-13 23:08:03 -0500

    updating server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 ﻿var express = require('express');
 var app = express();
 var port = 3000;
+var path = require('path');
 
 app.get("/", function (req, res) {
     //res.redirect('/public_html/index.html');
-    res.send('Hello World!');
+    //res.send('Hello World!');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, function () {
