187a8bfb374894f8b398fa354a686dbebbd6f07b
SDK <s.danyal.k@gmail.com>
2016-04-13 21:29:42 -0500

    updating server,js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 var port = 3000;
 
 app.get("/", function (req, res) {
-    res.redirect('../public_html/index.html');
+    //res.redirect('/public_html/index.html');
+    res.redirect('www.google.com');
 });
 
 app.listen(port);
